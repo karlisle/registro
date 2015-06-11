4402efe195c6f127b79ef7c1e53a85af4645db71
karlisle <inoue.sora@gmail.com>
2015-06-11 16:59:02 -0500

    Comenzando con la segunda parte 000

---
+++ control-de-acceso/estadisticas.py
@@ -0,0 +1,10 @@
+#!usr/bin/python
+
+########################################
+#	By: Kharl                          #
+#	Copyleft: Creative Commons         #
+#	Usa, modifica, distribuye          #
+#	y no olvides referenciar.          #
+#	El mundo es de los lo contruyen!!  #
+########################################
+
